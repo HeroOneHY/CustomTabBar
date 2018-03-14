2c5b2ba6edd7507aa85a4d9ee72cf4bd7a838eab
yanhe <yanhe@360.cn>
2018-03-14 16:10:47 +0800

    添加自定义tabbar

--- customtabbar/customtabbar/ViewController.m
+++ customtabbar/customtabbar/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "JMTabBarController.h"
+#import "JMConfig.h"
 
 @interface ViewController ()
 
@@ -16,9 +18,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    //初始化标题数组, 未选择图片数组, 选择图片数组, 控制器数组
+    NSMutableArray *titleArr = [NSMutableArray arrayWithObjects:@"首页",@"热点",@"doki",@"个人中心", nil];
+    NSMutableArray *imageNormalArr = [NSMutableArray arrayWithObjects:@"tab1_nor",@"tab2_nor",@"tab3_nor",@"tab4_nor", nil];
+    NSMutableArray *imageSelectedArr = [NSMutableArray arrayWithObjects:@"tab1_sel",@"tab2_sel",@"tab3_sel",@"tab4_sel", nil];
+    NSMutableArray *controllersArr = [NSMutableArray array];
+    for (int i = 0; i < titleArr.count; i++) {
+        
+        if (i == 0) {
+            ViewController *vc = [[ViewController alloc] init];
+            UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+            [controllersArr addObject:nav];
+        } else {
+            UIViewController *vc = [[UIViewController alloc] init];
+            vc.view.backgroundColor = kRandomColor;
+            UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+            
+            [controllersArr addObject:nav];
+        }
+    }
+    
+    //初始化配置信息
+    JMConfig *config = [JMConfig config];
+    
+    JMTabBarController *tabBarVc = [[JMTabBarController alloc] initWithTabBarControllers:controllersArr NorImageArr:imageNormalArr SelImageArr:imageSelectedArr TitleArr:titleArr Config:config];
+    [self presentViewController:tabBarVc animated:NO completion:nil];
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
